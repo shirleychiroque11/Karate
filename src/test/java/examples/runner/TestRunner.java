4cf5e20be8ece1293c59f00aa528d335d5e60ffe
shirleychiroque11 <161527409+shirleychiroque11@users.noreply.github.com>
2025-07-12 23:16:16 +0000

    test

--- src/test/java/examples/runner/TestRunner.java
+++ src/test/java/examples/runner/TestRunner.java
@@ -10,7 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import java.io.File;
 import net.masterthought.cucumber.Configuration;
